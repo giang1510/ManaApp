5b11c8425fa787f21ae9c71123bd5cf396d5c982
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-14 00:00:08 +0200

    Add REST-Service-Interface

---
+++ ManaApp/ManaApp/InterfaceCrossPlatform/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManaApp.InterfaceCrossPlatform
+{
+    interface IRestService
+    {
+        Task<string> RefreshDataAsync();
+        Task<string> getPage(string relativePath);
+        Task<string> login(string username, string password);
+        //Task<string> register(User user);
+    }
+}
