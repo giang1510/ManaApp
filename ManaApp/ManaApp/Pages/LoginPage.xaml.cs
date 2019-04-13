9293e76b052f5cdac510ae200ffc3c5231c1768b
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-13 19:56:20 +0200

    Add LoginPage

---
+++ ManaApp/ManaApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ManaApp.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void SignUp(object sender, EventArgs e)
+        {
+            //Navigation.PushModalAsync(new RegisterPage());
+        }
+
+        private async void Login(object sender, EventArgs e)
+        {
+            //IRestService service = new RestService();
+            //string result = await service.login(usernameEntry.Text, passwordEntry.Text);
+            //messageLabel.Text = result;
+        }
+    }
+}
