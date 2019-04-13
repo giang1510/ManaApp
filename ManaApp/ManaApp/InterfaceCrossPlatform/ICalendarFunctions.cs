0ada11347636835cfdc35b1fde3ddadf5899c630
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-14 00:01:51 +0200

    Add interface for calendar functions

---
+++ ManaApp/ManaApp/InterfaceCrossPlatform/ICalendarFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManaApp.InterfaceCrossPlatform
+{
+    interface ICalendarFunctions
+    {
+        string showAllCalendars();
+        string getEventsFromDevice(DateTime beginTime, DateTime endTime);
+        //ScheduleAppointmentCollection getAppList(DateTime beginTime, DateTime endTime);
+    }
+}
