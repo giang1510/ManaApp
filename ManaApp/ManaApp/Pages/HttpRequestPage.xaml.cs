c77d0cc30703c931fef954c7b89e2bf8261b3034
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-13 23:14:24 +0200

    Add HttpRequestPage

---
+++ ManaApp/ManaApp/Pages/HttpRequestPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ManaApp.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HttpRequestPage : ContentPage
+	{
+		public HttpRequestPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void HttpRequestClicked(Object sender, EventArgs e)
+        {
+            //IRestService service = new RestService();
+            //string result = await service.RefreshDataAsync();
+            //httpResponseView.Text = result;
+        }
+
+        private async void ToPageClicked(Object sender, EventArgs e)
+        {
+            //IRestService service = new RestService();
+            //string result = await service.getPage(relativePath.Text);
+            //httpResponseView.Text = result;
+        }
+    }
+}
