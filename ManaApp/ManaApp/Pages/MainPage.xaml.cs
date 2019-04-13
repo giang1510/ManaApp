16600de4fa6375fa3f3d1fc3e6a9e01020625f72
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-13 19:39:02 +0200

    Add buttons to main page to navigate to all pages

--- ManaApp/ManaApp/Pages/MainPage.xaml.cs
+++ ManaApp/ManaApp/Pages/MainPage.xaml.cs
@@ -13,5 +13,49 @@ namespace ManaApp
         {
             InitializeComponent();
         }
+
+        //private void ShowAllCalendarsClicked(object sender, EventArgs e)
+        //{
+        //    ICalendarFunctions calendarFunctions = DependencyService.Get<ICalendarFunctions>();
+        //    if (calendarFunctions != null)
+        //    {
+        //        textView.Text = calendarFunctions.showAllCalendars();
+        //    }
+        //}
+
+        //private void ShowEventsClicked(object sender, EventArgs e)
+        //{
+        //    ICalendarFunctions calendarFunctions = DependencyService.Get<ICalendarFunctions>();
+        //    if (calendarFunctions != null)
+        //    {
+        //        textView.Text = calendarFunctions.getEventsFromDevice(new DateTime(2017, 12, 1, 0, 0, 1), new DateTime(2017, 12, 31, 23, 59, 59));
+        //    }
+        //}
+
+        //private void LoadHttpPageClicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new PageHttpRequest());
+        //}
+
+        //private void LoadCalendarPageClicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new PageCalendar());
+        //}
+
+        //private void LoadLoginPageClicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new LoginPage());
+        //}
+
+        //private void LoadRegisterPageClicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new PageRegister());
+        //}
+
+        //private void LoadNewAppInfoPageClicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new PageNewAppInfo());
+        //}
+
     }
 }
