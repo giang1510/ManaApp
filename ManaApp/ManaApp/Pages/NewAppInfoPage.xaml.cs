a1acee1f9cd2534d70aeb4dd46dd6d24aed90d63
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-13 23:19:17 +0200

    Add NewAppInfoPage

---
+++ ManaApp/ManaApp/Pages/NewAppInfoPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ManaApp.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewAppInfoPage : ContentPage
+	{
+		public NewAppInfoPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void NewAppClicked(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void ToCalendarPageClicked(object sender, EventArgs e)
+        {
+            //TODO
+            Navigation.PushAsync(new CalendarPage());
+        }
+    }
+}
