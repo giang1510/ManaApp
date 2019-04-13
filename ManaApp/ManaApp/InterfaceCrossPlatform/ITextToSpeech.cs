be395445ebd3bd40cd0e03120d4cc442c5232c96
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-14 00:03:33 +0200

    Add interface for display text functions

---
+++ ManaApp/ManaApp/InterfaceCrossPlatform/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManaApp.InterfaceCrossPlatform
+{
+    interface ITextToSpeech
+    {
+        void speak(string text);
+    }
+}
