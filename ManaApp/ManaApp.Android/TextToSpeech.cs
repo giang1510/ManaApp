07899b875c8a69b073083198a7c75740dccf478e
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-14 13:55:57 +0200

    Add actual TextToSpeech for Android

---
+++ ManaApp/ManaApp.Android/TextToSpeech.cs
@@ -0,0 +1,16 @@
+﻿
+using Android.App;
+using Android.Widget;
+using ManaApp.InterfaceCrossPlatform;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FormApp2.Droid.TextToSpeech))]
+namespace FormApp2.Droid
+{
+    class TextToSpeech : ITextToSpeech
+    {
+        public void speak(string text)
+        {
+            Toast.MakeText(Application.Context, text, ToastLength.Long).Show();
+        }
+    }
+}
