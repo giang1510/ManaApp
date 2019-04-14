6323654c32a5378446ae271a5338f21c7e08e676
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-04-14 13:48:33 +0200

    Navigate from MainPage to all the Pages

--- ManaApp/ManaApp/Pages/MainPage.xaml.cs
+++ ManaApp/ManaApp/Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ManaApp.InterfaceCrossPlatform;
+using ManaApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,45 +18,45 @@ namespace ManaApp
 
         private void ShowAllCalendars(object sender, EventArgs e)
         {
-            //ICalendarFunctions calendarFunctions = DependencyService.Get<ICalendarFunctions>();
-            //if (calendarFunctions != null)
-            //{
-            //    textView.Text = calendarFunctions.showAllCalendars();
-            //}
+            ICalendarFunctions calendarFunctions = DependencyService.Get<ICalendarFunctions>();
+            if (calendarFunctions != null)
+            {
+                rawDataTextView.Text = calendarFunctions.showAllCalendars();
+            }
         }
 
         private void ShowEvents(object sender, EventArgs e)
         {
-            //ICalendarFunctions calendarFunctions = DependencyService.Get<ICalendarFunctions>();
-            //if (calendarFunctions != null)
-            //{
-            //    textView.Text = calendarFunctions.getEventsFromDevice(new DateTime(2017, 12, 1, 0, 0, 1), new DateTime(2017, 12, 31, 23, 59, 59));
-            //}
+            ICalendarFunctions calendarFunctions = DependencyService.Get<ICalendarFunctions>();
+            if (calendarFunctions != null)
+            {
+                rawDataTextView.Text = calendarFunctions.getEventsFromDevice(new DateTime(2017, 12, 1, 0, 0, 1), new DateTime(2017, 12, 31, 23, 59, 59));
+            }
         }
 
         private void LoadHttpPage(object sender, EventArgs e)
         {
-            //Navigation.PushModalAsync(new PageHttpRequest());
+            Navigation.PushModalAsync(new HttpRequestPage());
         }
 
         private void LoadCalendarPage(object sender, EventArgs e)
         {
-            //Navigation.PushModalAsync(new PageCalendar());
+            Navigation.PushModalAsync(new CalendarPage());
         }
 
         private void LoadLoginPage(object sender, EventArgs e)
         {
-            //Navigation.PushModalAsync(new LoginPage());
+            Navigation.PushModalAsync(new LoginPage());
         }
 
         private void LoadRegisterPage(object sender, EventArgs e)
         {
-            //Navigation.PushModalAsync(new PageRegister());
+            Navigation.PushModalAsync(new RegisterPage());
         }
 
         private void LoadNewAppInfoPage(object sender, EventArgs e)
         {
-            //Navigation.PushModalAsync(new PageNewAppInfo());
+            Navigation.PushModalAsync(new NewAppInfoPage());
         }
 
     }
