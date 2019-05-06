104534c9b9937086f7b1a7f0c8168ae6b6a88e4b
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-05-06 17:03:03 +0200

    Fix bug using UTC time

--- ManaApp/ManaApp/Model/ProviderAppointment.cs
+++ ManaApp/ManaApp/Model/ProviderAppointment.cs
@@ -6,9 +6,34 @@ namespace ManaApp.Model
 {
     public class ProviderAppointment
     {
+        // All datetime data from server is in UTC format (universal time),
+        // which is why it must be converted to local time
+        private DateTime startDateUTC;
+        private DateTime endDateUTC;
+
         public string appointment_name { get; set; }
-        public DateTime start_date { get; set; }
-        public DateTime end_date { get; set; }
+        public DateTime start_date
+        {
+            get
+            {
+                return startDateUTC.ToLocalTime();
+            }
+            set
+            {
+                startDateUTC = value.ToUniversalTime();
+            }
+        }
+        public DateTime end_date
+        {
+            get
+            {
+                return endDateUTC.ToLocalTime();
+            }
+            set
+            {
+                endDateUTC = value.ToUniversalTime();
+            }
+        }
         public string color { get; set; }
         public string notification { get; set; }
         public UserInfo user_info { get; set; }
